2e314af00d773edcabc50b5f425b4ed84774a418
Yury Dudin <cepob33@gmail.com>
2023-06-18 12:25:52 +0500

    ex64 complite

---
+++ ex64/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задайте значение N. Напишите программу, которая 
+//выведет все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+
+Console.WriteLine("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+void ForNtoOne (int n)
+{
+    if (n>0)
+    {
+        Console.Write(n+" ");
+        ForNtoOne(n-1);
+    }
+}
+
+ForNtoOne(n);
